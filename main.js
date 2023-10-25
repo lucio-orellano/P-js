3f88e1d601bc1da2382731e8b78e3e7d42bb43e0
lucio orellano <orellanolucio22@gmail.com>
2023-10-25 20:15:37 -0300

    segundo commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// alert("En nuestra tienda de indumentaria NACE te garantizamos una compra segura, agrega todos los productos que quieras a tu carrito y paga cuando estes listo!");
+
+// function selecCat() {
+//     let categorias = prompt("Que tipo de ropa estas buscando?, selecciona un tipo de prenda para ver nuestro catálogo: pantalones, buzos, remeras")
+
+//     if (categorias != "" && (categorias === "pantalones" || categorias === "remeras" || categorias === "buzos")) {
+//         alert(`genial! , a continuación te vamos mostrar nuestro stock en ${categorias}`);
+//       } else {
+//         alert("producto no encontrado.");
+//       }
+// }
+
+// selecCat(); 
+
+// function catalogo() {
+//     const productosPantalones = [
+//         { id: 1, nombre: "pantalon negro", precio: $7500 },
+//         { id: 2, nombre: "pantalon mom", precio: $7800 },
+//         { id: 3, nombre: "pantalon ripped", precio: $6200 },
+//     ]
+
+//     const productosRemeras = [
+//         { id: 1, nombre: "remera change", precio: $7500 },
+//         { id: 2, nombre: "remera unknown", precio: $7800 },
+//         { id: 3, nombre: "remera eternal", precio: $6200 },
+//         { id: 4, nombre: "remera caution", precio: $8200 },
+//         { id: 5, nombre: "remera babygirl", precio: $6000},
+//     ]
+
+//     const productosBuzos = [
+//         { id: 1, nombre: "buzo onward", precio: $7500 },
+//         { id: 2, nombre: "buzo trapstar", precio: $7800 },
+//         { id: 3, nombre: "buzo love", precio: $6200 },
+//         { id: 4, nombre: "buzo represent", precio: $8200 },
+//     ]
+// }
+
+
+
+
+
+
+
